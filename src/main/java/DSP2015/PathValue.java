d2cb42d25eef820851239a37b51dfff19d723e6f
barashe <eranbarash@gmail.com>
2015-08-06 15:12:34 +0300

    PathValue created - usable, but not finalized.

---
+++ src/main/java/DSP2015/PathValue.java
@@ -0,0 +1,103 @@
+package DSP2015;
+
+import org.apache.hadoop.io.*;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * Created by barashe on 8/6/15.
+ */
+public class PathValue implements Writable, WritableComparable<PathValue>{
+
+    private IntWritable count;
+    private IntWritable totalCount;
+    private IntWritable totalPathSlotXCount;
+    private IntWritable totalPathSlotYCount;
+    private IntWritable wordSlotXCount;
+    private IntWritable wordSlotYcount;
+    private BooleanWritable isFirst;
+
+
+    public PathValue(IntWritable count, IntWritable totalCount, IntWritable totalPathSlotXCount, IntWritable totalPathSlotYCount, IntWritable wordSlotXCount, IntWritable wordSlotYcount, BooleanWritable isFirst) {
+        this.count = count;
+        this.totalCount = totalCount;
+        this.totalPathSlotXCount = totalPathSlotXCount;
+        this.totalPathSlotYCount = totalPathSlotYCount;
+        this.wordSlotXCount = wordSlotXCount;
+        this.wordSlotYcount = wordSlotYcount;
+        this.isFirst = isFirst;
+    }
+
+    public PathValue(int count, int totalCount, int totalPathSlotXCount, int totalPathSlotYCount, int wordSlotXCount, int wordSlotYCount, boolean isFirst) {
+        this(new IntWritable(count), new IntWritable(totalCount), new IntWritable(totalPathSlotXCount), new IntWritable(totalPathSlotYCount), new IntWritable(wordSlotXCount), new IntWritable(wordSlotYCount), new BooleanWritable(isFirst));
+    }
+
+    public PathValue(int count, boolean isFirst) {
+        this(count, 0, 0, 0, 0, 0, isFirst);
+
+    }
+
+    /**
+     * Deep copies PathValue 'other' to a new PathValue
+     * @param other - a PathValue to copy
+     */
+    public PathValue(PathValue other){
+        this(other.getCount(), other.getTotalCount(), other.getTotalPathSlotXCount(), other.getTotalPathSlotYCount(), other.getWordSlotXCount(), other.getWordSlotYcount(), other.getIsFirst());
+    }
+
+    public IntWritable getCount() {
+        return new IntWritable(count.get());
+    }
+
+    public IntWritable getTotalCount() {
+        return new IntWritable(totalCount.get());
+    }
+
+    public IntWritable getTotalPathSlotXCount() {
+        return new IntWritable(totalPathSlotXCount.get());
+    }
+
+    public IntWritable getTotalPathSlotYCount() {
+        return new IntWritable(totalPathSlotYCount.get());
+    }
+
+    public IntWritable getWordSlotXCount() {
+        return new IntWritable(wordSlotXCount.get());
+    }
+
+    public IntWritable getWordSlotYcount() {
+        return new IntWritable(wordSlotYcount.get());
+    }
+
+    public BooleanWritable getIsFirst() {
+        return new BooleanWritable(isFirst.get());
+    }
+
+    public int compareTo(PathValue pathValue) {
+        return 0;
+    }
+
+
+    public void write(DataOutput dataOutput) throws IOException {
+        count.write(dataOutput);
+        totalCount.write(dataOutput);
+        totalPathSlotXCount.write(dataOutput);
+        totalPathSlotYCount.write(dataOutput);
+        wordSlotXCount.write(dataOutput);
+        wordSlotYcount.write(dataOutput);
+        isFirst.write(dataOutput);
+    }
+
+    public void readFields(DataInput dataInput) throws IOException {
+        count.readFields(dataInput);
+        totalCount.readFields(dataInput);
+        totalPathSlotXCount.readFields(dataInput);
+        totalPathSlotYCount.readFields(dataInput);
+        wordSlotXCount.readFields(dataInput);
+        wordSlotYcount.readFields(dataInput);
+        isFirst.readFields(dataInput);
+    }
+
+}
