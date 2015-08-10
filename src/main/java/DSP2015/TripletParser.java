d0cf4450c2fa0cf96bcd377329ccd037f0d8b61c
barashe <eranbarash@gmail.com>
2015-08-10 12:18:33 +0300

    Sort of a parser is implemented

--- src/main/java/DSP2015/TripletParser.java
+++ src/main/java/DSP2015/TripletParser.java
@@ -1,29 +1,164 @@
 package DSP2015;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by barashe on 8/9/15.
  */
 public class TripletParser {
 
+    private class Word{
+        private String word;
+        private String type;
+        private int parent;
+        private int index;
+
+        public Word(String word, String type, int parent, int index) {
+            this.word = word;
+            this.type = type;
+            this.parent = parent;
+            this.index = index;
+        }
+
+        public String getWord() {
+            return word;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public int getParent() {
+            return parent;
+        }
+
+        public boolean isRoot(){
+            return (parent ==0);
+        }
+
+        public boolean isNoun(){
+            return (type.charAt(0) == 'N' || type.equals("PRP"));
+        }
+
+        public boolean isVerb(){
+            return (type.charAt(0) == 'V');
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Word word = (Word) o;
+
+            return index == word.index;
+
+        }
+
+    }
+
+    private class ParsedLine{
+        private List<Word> words;
+        private int count;
+
+        public ParsedLine(String line) {
+            words = new ArrayList<Word>();
+            String[] tmp = line.split("\t");
+            count = Integer.parseInt(tmp[2]);
+            String[] wordsToParse = tmp[1].split(" ");
+            for(int i = 0; i < wordsToParse.length; i++){
+                String[] data = wordsToParse[i].split("/");
+                Word word = new Word(data[0], data[1], Integer.parseInt(data[3]), i);
+                words.add(word);
+            }
+        }
+
+
+        public Word getWord(int index){
+            return words.get(index);
+        }
+
+        public Word getParent(Word word){
+            if(word.getParent() == 0)
+                return null;
+            return words.get(word.getParent() - 1);
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        public int size(){
+            return words.size();
+        }
+    }
+
+    private List<String> paths;
+    private int count;
+
     public TripletParser() {
         count = 0;
-        path = "";
+        paths = null;
     }
 
     public int getCount() {
         return count;
     }
 
-    public String getPath() {
-        return path;
+    public List<String> getPath() {
+        return paths;
     }
 
-    private String path;
-    private int count;
+    private boolean isValid(ParsedLine pl, Word noun, Word otherNoun){
+        Word w = pl.getParent(noun);
+        while(w != null){
+            if(w.equals(otherNoun))
+                return false;
+            w = pl.getParent(w);
+        }
+        return true;
+    }
 
     public void parse(String line){
-        String[] tmp = line.split("\t");
-        path = tmp[1];
-        count = Integer.parseInt(tmp[2]);
+
+        ParsedLine pl = new ParsedLine(line);
+        Word word;
+        String path ="";
+        Word firstNoun = null, secondNoun= null;
+        for(int i = 0; i < pl.size(); i++){
+            word = pl.getWord(i);
+            if(i>0)
+                path += " ";
+            if(word.isRoot()) {
+                if (!word.isVerb()) {
+                    return;
+                }
+                path += word.getWord();
+            }
+            else {
+                if (word.isNoun()) {
+                    if(firstNoun == null) {
+                        firstNoun = word;
+                        path += "X";
+                    }
+                    else {
+                        secondNoun = word;
+                        path += "Y";
+                    }
+                }
+                else
+                    path += word.getWord();
+
+            }
+        }
+
+        if(firstNoun != null && secondNoun != null){
+            if(isValid(pl, firstNoun, secondNoun) && isValid(pl, secondNoun, firstNoun))
+                paths = new ArrayList<String>();
+                paths.add(path);
+        }
+        count = pl.getCount();
+
     }
 }
--- src/test/java/DSP2015/TripletParserTest.java
+++ src/test/java/DSP2015/TripletParserTest.java
@@ -16,8 +16,8 @@ public class TripletParserTest extends TestCase {
 
     public void testPath() throws Exception{
         TripletParser p = new TripletParser();
-        p.parse("skulk\tskulk/VB/xcomp/0 like/IN/prep/1 whipped/VBN/amod/4 cur/RP/pobj/2\t13\t1863,2\t1923,3\t1924,2\t1925,3\t1933,1\t1946,1\t1949,1");
-        Assert.assertEquals(p.getPath(), "skulk/VB/xcomp/0 like/IN/prep/1 whipped/VBN/amod/4 cur/RP/pobj/2");
+        p.parse("slapped\tslapped/VBD/ROOT/0 him/PRP/dobj/1 on/IN/prep/1 cheek/NN/pobj/3\t85\t1828,1\t1873,1\t1884,2\t1907,1\t1911,1\t1920,1\t1932,2\t1933,4\t1934,1\t1936,1\t1941,1\t1943,2\t1944,1\t1945,9\t1946,3\t1947,1\t1948,1\t1949,2\t1950,2\t1951,1\t1952,1\t1956,3\t1961,1\t1962,1\t1964,2\t1967,1\t1973,2\t1974,2\t1976,1\t1978,3\t1979,2\t1980,1\t1981,1\t1982,2\t1983,1\t1985,2\t1986,1\t1987,1\t1988,1\t1991,2\t1992,1\t1993,2\t1994,1\t1998,1\t1999,2\t2001,2\t2002,2\t2003,1\t2004,1\t2006,2\t2008,1");
+        Assert.assertEquals("slapped X on Y", p.getPath().get(0));
     }
 
 }
